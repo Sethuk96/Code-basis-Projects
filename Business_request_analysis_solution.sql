3a34b35e47cb635eb6a11f33e85b18955271db0d
sethuraman kannan <111210958+Sethuk96@users.noreply.github.com>
2024-02-27 18:53:44 +0530

    Update Business_request_analysis_solution.sql

--- Business_request_analysis_solution.sql
+++ Business_request_analysis_solution.sql
@@ -148,7 +148,7 @@ report_2 as (
         product_name
 )
 select 
-    product_name,category,
+    product_name,
 CONCAT(FORMAT(IR, 2), '%') AS IR_percentage
 from 
     report_2 order by IR desc limit 5;
